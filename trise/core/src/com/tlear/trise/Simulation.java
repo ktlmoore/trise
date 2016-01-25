cacc731376c2b888f5320daafedf32f5e8a58179
tlear <tom@tlear.co.uk>
2016-01-25 13:48:21 +0000

    Simulation now displays mode information and hides the graph if not requested

--- trise/core/src/com/tlear/trise/Simulation.java
+++ trise/core/src/com/tlear/trise/Simulation.java
@@ -24,84 +24,93 @@ public class Simulation {
 	private Map<Integer, Integer> timeMap;
 	private int prevKeyframe, nextKeyframe;
 	private int time;
-	private TrackedGraph<Vector2> g; 
-	
-	public Simulation() {
+	private TrackedGraph<Vector2> g;
+
+	private TRISE parent;
+
+	public Simulation(TRISE parent) {
+		this.parent = parent;
+
 		createSim();
-		
+
 		prevKeyframe = 0;
 		nextKeyframe = 0;
-		
+
 		time = 0;
-		
+
 		Triple<Integer, Integer, TrackedGraph<Vector2>> next = env.getNextKeyframe(timeMap);
 		timeMap.put(next.fst, next.snd);
 		g = next.thd;
-		
+
 		nextKeyframe++;
 	}
-	
+
 	public void update() {
-//		System.out.println("UPDATE SIMULATION");
-//		System.out.println("PREV: " + prevKeyframe + ", NEXT: " + nextKeyframe);
-//		System.out.println("TIME MAP: " + timeMap.toString());
-		
-//		System.out.println(nextKeyframe);
-		
+		// System.out.println("UPDATE SIMULATION");
+		// System.out.println("PREV: " + prevKeyframe + ", NEXT: " +
+		// nextKeyframe);
+		// System.out.println("TIME MAP: " + timeMap.toString());
+
+		// System.out.println(nextKeyframe);
+
 		if (timeMap.get(nextKeyframe) == time) {
 			prevKeyframe = nextKeyframe;
-			
+
 			Triple<Integer, Integer, TrackedGraph<Vector2>> next = env.getNextKeyframe(timeMap);
 			timeMap.put(next.fst, next.snd);
 			g = next.thd;
-			
+
 			nextKeyframe++;
 		}
-		
-//		System.out.println(time);
-		
+
+		// System.out.println(time);
+
 		time++;
 		env.update(timeMap, prevKeyframe, time, nextKeyframe);
 	}
-	
+
 	public void draw(ShapeRenderer sr, SpriteBatch batch) {
-		
+
 		BitmapFont font = new BitmapFont();
-        font.setColor(Color.WHITE);
-		
-		
-		sr.begin(ShapeType.Line);
-		sr.setColor(0, 0, 0, 1);
-		for (Edge<Vector2> e : g.getEdges()) {
-			sr.line(e.fst.getValue(), e.snd.getValue());
-		}
-		sr.end();
-		
-		sr.begin(ShapeType.Filled);
-		sr.setColor(1, 1, 0, 1);
-		for (Node<Vector2> n : g.getNodes()) {
-			if (g.visited(n)) {
-				sr.setColor(0, 0, 1, 1);
-			} else {
-				sr.setColor(1, 1, 0, 1);
+		font.setColor(Color.WHITE);
+
+		// Draw the graph
+		if (parent.showGraph) {
+			sr.begin(ShapeType.Line);
+			sr.setColor(0, 0, 0, 1);
+			for (Edge<Vector2> e : g.getEdges()) {
+				sr.line(e.fst.getValue(), e.snd.getValue());
 			}
-			Vector2 v = n.getValue();
-			sr.circle(v.x, v.y, 5);
+			sr.end();
+			sr.begin(ShapeType.Filled);
+			sr.setColor(1, 1, 0, 1);
+			for (Node<Vector2> n : g.getNodes()) {
+				if (g.visited(n)) {
+					sr.setColor(0, 0, 1, 1);
+				} else {
+					sr.setColor(1, 1, 0, 1);
+				}
+				Vector2 v = n.getValue();
+				sr.circle(v.x, v.y, 5);
+			}
+			sr.end();
 		}
-		sr.end();
 
 		env.draw(sr, batch);
-		
-		System.out.println(g.getExploredNodes().size() + " / " + g.getNodes().size());
-		
+
+		// System.out.println(g.getExploredNodes().size() + " / " +
+		// g.getNodes().size());
+
 		batch.begin();
 		font.draw(batch, (float) (100 * g.getExploredNodes().size() / g.getNodes().size()) + "%", 600, 50);
+		font.draw(batch, parent.modeEdit ? "EDIT" : "", 600, 100);
+		font.draw(batch, parent.modeSim ? "SIMULATING" : "", 600, 150);
 		batch.end();
 	}
-	
+
 	private void createSim() {
 		env = new Environment();
-		
+
 		StaticObstacle o = new StaticObstacle(100, 100, 100, 100);
 		env.placeObstacle(o);
 		o = new StaticObstacle(300, 300, 100, 100);
@@ -110,26 +119,25 @@ public class Simulation {
 		env.placeObstacle(o);
 		o = new StaticObstacle(300, 100, 100, 100);
 		env.placeObstacle(o);
-		
+
 		StaticGoal g = new StaticGoal(450, 450, 20, 20);
 		env.placeGoal(g);
-		
+
 		Agent a = new Agent(10, 10, 25, 25, env);
 		env.placeAgent(a);
-		
-		
-//		g = new StaticGoal(250, 250, 20, 20);
-//		env.placeGoal(g);
-//		g = new StaticGoal(450, 250, 20, 20);
-//		env.placeGoal(g);
-//		g = new StaticGoal(250, 450, 20, 20);
-//		env.placeGoal(g);
-//		g = new StaticGoal(250, 50, 20, 20);
-//		env.placeGoal(g);
-//		g = new StaticGoal(50, 250, 20, 20);
-//		env.placeGoal(g);
-		
-//		System.out.println("Creating sim: " + env);
+
+		// g = new StaticGoal(250, 250, 20, 20);
+		// env.placeGoal(g);
+		// g = new StaticGoal(450, 250, 20, 20);
+		// env.placeGoal(g);
+		// g = new StaticGoal(250, 450, 20, 20);
+		// env.placeGoal(g);
+		// g = new StaticGoal(250, 50, 20, 20);
+		// env.placeGoal(g);
+		// g = new StaticGoal(50, 250, 20, 20);
+		// env.placeGoal(g);
+
+		// System.out.println("Creating sim: " + env);
 		timeMap = new LinkedHashMap<>();
 		timeMap.put(0, 0);
 	}
